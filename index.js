a04ba6b0d5587ebc4db7aea78fd0b33c616d28d7
ar-arif <just.ar.arif@gmail.com>
2022-01-23 16:20:46 +0600

    done

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
+"use strict";
 const cheerio = require("cheerio");
 const axios = require("axios");
-const reader = require('xlsx')
+const xlsx = require("xlsx");
+const prompt = require("prompt-sync")();
+const fs = require("fs");
 
 // global variable
-var searchURL
-var links = []
-var extractData = []
-const file = reader.readFile('./data.xlsx')
-
-// Reading our test file
-readDataFromFile(file)
-function readDataFromFile(file){
-	let data = []
-	const sheets = file.SheetNames
-	for (let i = 0; i < sheets.length; i++) {
-		const temp = reader.utils.sheet_to_json(
-			file.Sheets[file.SheetNames[i]])
-		temp.forEach((res) => {
-			data.push(res)
-		})
-	}
-
-	searchURL = data[0].search_url
-}
-function writeDataToFile(data, file){
-	const outputFileName = "output-" + new Date().valueOf();
-	const ws = reader.utils.json_to_sheet(data)
-	reader.utils.book_append_sheet(file,ws, outputFileName)
-	// Writing to our file
-	reader.writeFile(file,'./data.xlsx')
-}
-	
-
-
-
-
-
-
-
-
-
+// const searchURL = "https://www.affordablehousing.com/jacksonville-fl-32209/";
+const searchURL = prompt("Enter the search URL: ");
+const links = [];
+const extractData = [];
 
 // Request
 axios.get(searchURL).then((res) => {
@@ -48,37 +18,49 @@ axios.get(searchURL).then((res) => {
 	let $ = cheerio.load(body);
 
 	// BigCardList
-	$("#MainContentPlaceHolder_Panel1 .tnresult--card.jq-cardhover").each((i, element) => {
-		const item = $(element).find(".relative").find("a").attr("href").trim()
-		links.push(item)
-	})
+	$("#MainContentPlaceHolder_Panel1 .tnresult--card.jq-cardhover").each(
+		(i, element) => {
+			const item = $(element)
+				.find(".relative")
+				.find("a")
+				.attr("href")
+				.trim();
+			links.push(item);
+		}
+	);
 
 	// SmallCardList
-	$("#MainContentPlaceHolder_Panel1 .tnresult--small--card.bottom_card").each((i, element) => {
-		const item = $(element).find("a").attr("href").trim()
-		links.push(item)
-	})
-	
-
-	links.forEach((element) => {
+	$("#MainContentPlaceHolder_Panel1 .tnresult--small--card.bottom_card").each(
+		(i, element) => {
+			const item = $(element).find("a").attr("href").trim();
+			links.push(item);
+		}
+	);
+	let count = 1;
+	links.forEach((element, index) => {
 		axios.get(element).then((res) => {
-			let body = res.data
+			let body = res.data;
 			let $ = cheerio.load(body);
 
-			let phone = $(".inquiry--hdr--lt > .prem--owner--phn > p > a").html().trim()
-			let contact = $(".inquiry--hdr--lt > h4").html().trim()
-			let url = element
+			let phone = $(".inquiry--hdr--lt > .prem--owner--phn > p > a")
+				.html()
+				.trim();
+			let contact = $(".inquiry--hdr--lt > h4").html().trim();
+			let url = element;
 
-			extractData.push({contact, phone, url})
+			extractData.push({ Contact: contact, Phone: phone, URL: url });
 
+			console.log("[" + count + "/" + links.length + "]");
+			count += 1;
 
-		})
-	})
-
-	writeDataToFile(extractData, file)
-
-	
-	
+			if (extractData.length == links.length) {
+				// fs.writeFileSync("New_Data.json", JSON.stringify(extractData));
+				const newWB = xlsx.utils.book_new();
+				const newWS = xlsx.utils.json_to_sheet(extractData);
+				xlsx.utils.book_append_sheet(newWB, newWS, "New_Data");
+				xlsx.writeFile(newWB, "New_Data.xlsx");
+				console.log("Done ✔");
+			}
+		});
+	});
 });
-
-
--- xlsx_test.js
+++
@@ -1,9 +0,0 @@
-// Requiring the module
-const XLSX = require("xlsx");
-
-var wb = XLSX.read("./data.xlsx");
-var ws = wb.Sheets[wb.SheetNames[0]];
-
-console.log(ws);
-// XLSX.utils.sheet_add_aoa(ws, [["new data", 54, 54, 45]]);
-// XLSX.writeFile(wb, "./data.xlsx");
